49d56357cb358f9518873ef26a77caaa88a88863
Philip Couling <couling@gmail.com>
2019-03-26 15:32:46 +0000

    Working filter config

--- logban.py
+++ logban.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+from getopt import getopt
+import os
 import os.path
 import pyinotify
+import re
+import sys
 
 
 class EventMaster(object):
@@ -15,6 +19,7 @@ class EventMaster(object):
         except KeyError:
             EventMaster.event_actions[event] = [action]
 
+    @staticmethod
     def publish_event(event, **details):
         try:
             for action in EventMaster.event_actions[event]:
@@ -25,101 +30,158 @@ class EventMaster(object):
 
 class FileMonitor(pyinotify.ProcessEvent):
 
-    def __init__(self):
-        self.logs = {}
-        self._wm = pyinotify.WatchManager()
-        self._notifier = pyinotify.Notifier(self._wm, self)
-        self._wd_dict = {}
+    __notify_events = pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY
+    __wd_dict = {}
+    __wm = pyinotify.WatchManager()
+
+    logs = {}
+
+    @staticmethod
+    def register_log(path, position=0):
+        if path not in FileMonitor.logs:
+            FileMonitor.logs[path] = __MonitoredFile(path, position)
+            directory = os.path.dirname(path)
+            if directory not in FileMonitor.__wd_dict:
+                FileMonitor.__wd_dict[directory] = \
+                    FileMonitor.__wm.add_watch(directory, FileMonitor.__notify_events, rec=True)
+
+    @staticmethod
+    def loop():
+        for log, watcher in FileMonitor.logs.items():
+            lines = watcher.read_new_lines(auto_reset=False)
+            if len(lines) > 0:
+                EventMaster.publish_event(log, lines=lines)
+        notifier = pyinotify.Notifier(FileMonitor.__wm, __INotifyEvent())
+        notifier.loop()
+
+    @staticmethod
+    def close():
+        for log in FileMonitor.logs:
+            log.close()
+
+
+class _FileMonitor__INotifyEvent(pyinotify.ProcessEvent):
 
     def process_IN_CREATE(self, event):
-        if not event.dir and event.pathname in self.logs:
-            self.logs[event.pathname].reset()
+        if not event.dir and event.pathname in FileMonitor.logs:
+            FileMonitor.logs[event.pathname].open()
 
     def process_IN_DELETE(self, event):
-        if not event.dir and event.pathname in self.logs:
-            self.logs[event.pathname].close()
+        if not event.dir and event.pathname in FileMonitor.logs:
+            FileMonitor.logs[event.pathname].close()
 
     def process_IN_MODIFY(self, event):
-        if not event.dir and event.pathname in self.logs:
-            self.logs[event.pathname].notify_change()
-
-    #def process_default(self,event):
-    #    print(event)
-
-    def register_log(self, path, position=0):
-        path = os.path.realpath(path)
-        if path not in self.logs:
-            self.logs[path] = _MonitoredFile(path, position)
-            directory = os.path.dirname(path)
-            if directory not in self._wd_dict:
-                self._wd_dict[directory] = self._wm.add_watch(directory,
-                        pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY,
-                        rec=True)
-
-    def loop(self):
-        for log, watcher in self.logs.items():
-            watcher.notify_change()
-        self._notifier.loop()
+        if not event.dir and event.pathname in FileMonitor.logs:
+            lines = FileMonitor.logs[event.pathname].read_new_lines()
+            if len(lines) > 0:
+                EventMaster.publish_event(event.pathname, lines=lines)
 
 
+class _FileMonitor__MonitoredFile(object):
 
-class _MonitoredFile(object):
+    def __init__(self, file_path, position=0):
+        self.file_path = file_path
+        self.file = None
+        self.open(position)
 
-    def __init__(self, log_file, position=0):
-        self.log_file = log_file
-        self.open_file = None
-        self._open_file()
+    def get_pos(self):
+        return self.file.tell()
 
-    def reset(self):
-        self.close()
-        self._open_file()
-
-    def notify_change(self):
-        pos = self.open_file.tell()
-        line = self.open_file.readline()
-        if line == '':
+    def read_new_lines(self, auto_reset=True):
+        if self.file == None:
+            return []
+        pos = self.get_pos()
+        line = self.file.readline()
+        if line == '' and auto_reset:
             self.reset()
-            pos = self.open_file.tell()
-            line = self.open_file.readline()
+            pos = self.file.tell()
+            line = self.file.readline()
+        lines = []
         while line != '':
             if line[-1:] == '\n':
-                EventMaster.publish_event(self.log_file, line=line[:-1])
-                pos = self.open_file.tell()
-                line = self.open_file.readline()
+                lines.append(line[:-1])
+                pos = self.file.tell()
+                line = self.file.readline()
             else:
-                print("Self reset %s" % self.log_file)
                 # if we get a partial line we seek back to the start of the line
-                self.open_file.seek(pos)
+                self.file.seek(pos)
                 line = ''
+        return lines
 
-    def _open_file(self):
-        if os.path.isfile(self.log_file):
-            self.open_file = open(self.log_file, 'r')
+    def open(self, position=0):
+        self.close()
+        if os.path.isfile(self.file_path):
+            self.file = open(self.file_path, 'r')
+            if position != 0:
+                self.file.seek(position, 0)
 
     def close(self):
-        if self.open_file is not None:
-            self.open_file.close()
-            self.open_file = None
-
+        if self.file is not None:
+            self.file.close()
+            self.file = None
 
 
 class LogFilter(object):
     
-    def __init__(self, event, pattern):
-        global event_actions
+    def __init__(self, event, log_path, pattern):
         self.event = event
-        self.pattern = pattern.compile()
+        self.pattern = re.compile(pattern)
+        self.log_path = log_path
 
-    def filter_line(log, line):
-        found = self.pattern.search(line)
-        if found is not None:
-            EventMaster.publish_event(event, lines=[line], address=found.group('host'), user=found('user'))
+    def filter_line(self, log, lines, ** named_params):
+        for line in lines:
+            found = self.pattern.search(line)
+            if found is not None:
+                EventMaster.publish_event(self.event,
+                                          log=self.log_path,
+                                          lines=[line],
+                                          addresses=found.group('host'),
+                                          user=found.group('user'))
+
+class Configuration(object):
+
+    @staticmethod
+    def load_config():
+        opt_list, _ = getopt(sys.argv[1:], '', ['config='])
+        opt_list = { option[2:]: value for option, value in opt_list }
+        return Configuration.load_config_files(**opt_list)
+
+    @staticmethod
+    def load_config_files(config='/etc/logban', **other_options):
+        filters_conf = Configuration.load_filter_files(config)
+        for filter_conf in filters_conf:
+            log_path = os.path.abspath(filter_conf['log_path'])
+            # Enable monitoring of this log
+            FileMonitor.register_log(log_path)
+            # Add a new filter for this log
+            filter = LogFilter(**filter_conf)
+            EventMaster.register_action(log_path, filter.filter_line)
+
+    @staticmethod
+    def load_filter_files(config):
+        filter_re = re.compile(r'^ *(?P<log_path>[^#|]+) *\| *(?P<event>[^ |]+) *\|(?P<pattern>.+) *$')
+        config = os.path.join(config, 'filters')
+        filters = []
+        filter_files = os.listdir(config)
+        for filter_path in filter_files:
+            filter_path = os.path.join(config, filter_path)
+            if filter_path.endswith('.conf'):
+                with open(filter_path) as filter_file:
+                    for line in filter_file:
+                        match = filter_re.match(line)
+                        if match is not None:
+                            filter = {
+                                    'log_path': match.group('log_path'),
+                                    'event': match.group('event'),
+                                    'pattern': match.group('pattern').format(
+                                        host=r'(?P<host>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)',
+                                        user=r'(?P<user>.*)'
+                                    )
+                                }
+                            filters.append(filter)
+        return filters
 
 
-
-
-
-file_monitor = FileMonitor()
-file_monitor.loop()
-
-
+Configuration.load_config()
+EventMaster.register_action('test_log', lambda event, ** args: print("Triggered: %s: %s" % (event, args)))
+FileMonitor.loop()
