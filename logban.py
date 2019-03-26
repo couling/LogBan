7fd9ae993eafd8ca4b6a31d2f4128b867e3ea83a
Philip Couling <couling@gmail.com>
2019-03-26 10:18:49 +0000

    Progress

--- logban.py
+++ logban.py
@@ -4,7 +4,26 @@ import os.path
 import pyinotify
 
 
-class FileWatcher(pyinotify.ProcessEvent):
+class EventMaster(object):
+
+    event_actions = {}    
+
+    @staticmethod
+    def register_action(event, action):
+        try:
+            EventMaster.event_actions[event].append(action)
+        except KeyError:
+            EventMaster.event_actions[event] = [action]
+
+    def publish_event(event, **details):
+        try:
+            for action in EventMaster.event_actions[event]:
+                action(event, **details)
+        except KeyError:
+            EventMaster.event_actions[event] = []
+
+
+class FileMonitor(pyinotify.ProcessEvent):
 
     def __init__(self):
         self.logs = {}
@@ -27,18 +46,15 @@ class FileWatcher(pyinotify.ProcessEvent):
     #def process_default(self,event):
     #    print(event)
 
-    def register_filter(self, path, action):
-        directory = os.path.dirname(path)
-        if directory not in self._wd_dict:
-            self._wd_dict[directory] = self._wm.add_watch(directory, 
-                    pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY, 
-                    #pyinotify.ALL_EVENTS,
-                    rec=True)
+    def register_log(self, path, position=0):
         path = os.path.realpath(path)
-        try:
-            self.logs[path].append(action)
-        except KeyError:
-            self.logs[path] = MonitoredLog(log_file=path, actions=[action])
+        if path not in self.logs:
+            self.logs[path] = _MonitoredFile(path, position)
+            directory = os.path.dirname(path)
+            if directory not in self._wd_dict:
+                self._wd_dict[directory] = self._wm.add_watch(directory,
+                        pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY,
+                        rec=True)
 
     def loop(self):
         for log, watcher in self.logs.items():
@@ -47,10 +63,9 @@ class FileWatcher(pyinotify.ProcessEvent):
 
 
 
-class MonitoredLog(object):
+class _MonitoredFile(object):
 
-    def __init__(self, log_file, position=0, actions=[]):
-        self.actions = actions
+    def __init__(self, log_file, position=0):
         self.log_file = log_file
         self.open_file = None
         self._open_file()
@@ -68,8 +83,7 @@ class MonitoredLog(object):
             line = self.open_file.readline()
         while line != '':
             if line[-1:] == '\n':
-                for action in self.actions:
-                    action(line[:-1])
+                EventMaster.publish_event(self.log_file, line=line[:-1])
                 pos = self.open_file.tell()
                 line = self.open_file.readline()
             else:
@@ -87,8 +101,25 @@ class MonitoredLog(object):
             self.open_file.close()
             self.open_file = None
 
-file_watcher = FileWatcher()
-file_watcher.register_filter("./foo/bar", lambda line: print("./foo/bar line: %s" % line))
-file_watcher.loop()
+
+
+class LogFilter(object):
+    
+    def __init__(self, event, pattern):
+        global event_actions
+        self.event = event
+        self.pattern = pattern.compile()
+
+    def filter_line(log, line):
+        found = self.pattern.search(line)
+        if found is not None:
+            EventMaster.publish_event(event, lines=[line], address=found.group('host'), user=found('user'))
+
+
+
+
+
+file_monitor = FileMonitor()
+file_monitor.loop()
 
 
